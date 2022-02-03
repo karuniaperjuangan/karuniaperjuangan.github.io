b7ae55e76464fa1e3bc5363ea52386a72953120b
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2022-02-03 09:57:38 +0700

    update web

--- simasterwallpaper/flutter_service_worker.js
+++ simasterwallpaper/flutter_service_worker.js
@@ -17,19 +17,15 @@ const RESOURCES = {
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/NOTICES": "7e323ff39b0663d679aa8202140fb5c5",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"canvaskit/canvaskit.js": "43fa9e17039a625450b6aba93baf521e",
-"canvaskit/canvaskit.wasm": "04ed3c745ff1dee16504be01f9623498",
-"canvaskit/profiling/canvaskit.js": "f3bfccc993a1e0bfdd3440af60d99df4",
-"canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "9a3d1c567ec2a5ce15c9572f72cbaf34",
-"/": "9a3d1c567ec2a5ce15c9572f72cbaf34",
-"main.dart.js": "6f4e7a10b62fda91899674bdf6bf35a7",
-"manifest.json": "05b922e789802935c2a8ad45d9b39ba9",
+"index.html": "db5013a3d81794775851a5b22dfef26a",
+"/": "db5013a3d81794775851a5b22dfef26a",
+"main.dart.js": "ed811d1e01300672342f9d8259b8abbf",
+"manifest.json": "73c711243d83c72ddf56082232f63117",
 "version.json": "7bf671a9c1e8d3ba9b1c448b92d366b8"
 };
 
